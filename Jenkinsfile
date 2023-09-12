ef674985827be92917e5d8c7ea370800de1674c9
XMars <truong.pham2@sotatek.com>
2023-09-12 11:09:33 +0700

    up

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,6 +12,7 @@ node {
         + ' -e "POSTGRES_DB=postgres"'
         + ' --network odoo-cicd-net' 
         + ' -p 15430:5432'){c ->
+            sh 'docker logs ${c.id}'
             sh './odoo-docker/scripts/build.sh'
             
         }
--- odoo-docker/scripts/build.sh
+++ odoo-docker/scripts/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 docker ps -a
-docker logs ${c.id}
 ./odoo-docker/scripts/wait-for-it.sh localhost:15430 -t 30
 which psql
+cat /etc/os-release
 psql -h localhost:15430 -U odoo
-// while !</dev/tcp/db/5432; do sleep 1; done;
 ls -lah /
 echo $POSTGRES_DB $POSTGRES_PASSWORD
 psql -U odoo
