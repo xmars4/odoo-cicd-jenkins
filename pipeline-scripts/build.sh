1863bd46ac11d3eab6c1b75339b20a3c0f43e7c1
XMars <truong.pham2@sotatek.com>
2023-09-14 16:57:04 +0700

    up

--- pipeline-scripts/build.sh
+++ pipeline-scripts/build.sh
@@ -28,11 +28,11 @@ function update_config_file {
     # we use log to analytic error, so log_level should be 'error'
     # remove old log level command
     sed -i "s/^\s*log_level\s*.*//g" $ODOO_CONFIG_FILE
-    echo "\nlog_level = error" >>$ODOO_CONFIG_FILE
+    echo -e "\nlog_level = error" >>$ODOO_CONFIG_FILE
 
     # replace old command argument
     sed -i "s/^\s*command\s*.*//g" $ODOO_CONFIG_FILE
-    echo "\ncommand = -i "${EXTRA_ADDONS}" --test-enable --test-tags "${EXTRA_ADDONS}"" >>$ODOO_CONFIG_FILE
+    echo -e "\ncommand = -i "${EXTRA_ADDONS}" --test-enable --test-tags "${EXTRA_ADDONS}"" >>$ODOO_CONFIG_FILE
 }
 update_config_file
 
