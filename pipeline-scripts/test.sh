0c1d2b14b9443eeba2e836b17840aaf7f3f7609d
XMars <truong.pham2@sotatek.com>
2023-09-18 11:56:34 +0700

    up

--- pipeline-scripts/test.sh
+++ pipeline-scripts/test.sh
@@ -11,17 +11,19 @@ function analyze_log {
     # so no need to analyze log anymore
     docker exec $ODOO_CONTAINER_ID sh -c "[ -f ${LOG_FILE} ]"
     if [ $? != 0 ]; then
-        return 1
+        return 0
     fi
 
     docker exec $ODOO_CONTAINER_ID sh -c "grep -m 1 -P '^[0-9-\s:,]+ERROR' $LOG_FILE"
-    return $?
+    if [ $? -eq 0 ]; then
+        return 1
+    fi
+    return 0
 }
 
 function send_error_notice_to_dev {
-    analyze_log_result=$(analyze_log)
-    show_separator "your result here ${analyze_log_result}"
-    if [ "$analyze_log_result" -eq 0 ]; then
+    analyze_log
+    if [ $? -ne 0 ]; then
         send_message $TELEGRAM_BOT_TOKEN $TELEGRAM_CHANNEL_ID "ez bro, i got it after first tried"
     fi
 }
