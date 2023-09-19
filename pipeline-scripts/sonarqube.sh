4104ee432d5690805309486be1417b58178dfdab
XMars <truong.pham2@sotatek.com>
2023-09-19 09:46:38 +0700

    update

--- pipeline-scripts/sonarqube.sh
+++ pipeline-scripts/sonarqube.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source "${WORKSPACE}/pipeline-scripts/utils.sh"
+show_separator "hey u ${sonarqubeScannerHome}"
 
 ${sonarqubeScannerHome}/bin/sonar-scanner -e -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectName=sonarqube-odoo -Dsonar.projectKey=GS -Dsonar.projectBaseDir=\"${ODOO_WORKSPACE}/extra-addons\"
