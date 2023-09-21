5e17d3a38f326f2d649040e8e68a67058906ff02
XMars <truong.pham2@sotatek.com>
2023-09-21 17:27:01 +0700

    up

--- pipeline-scripts/deploy.sh
+++ pipeline-scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 source "${WORKSPACE}/pipeline-scripts/utils.sh"
 
-ssh "${server_username}"@"${server_host}" -i "${server_privatekey}" "cd \"${server_extra_addons_path}\" && git pull && cd \"${server_docker_compose_path}\" && docker compose restart"
+# ssh "${server_username}"@"${server_host}" -i "${server_privatekey}" "cd \"${server_extra_addons_path}\" && git pull && cd \"${server_docker_compose_path}\" && docker compose restart"
 
-ssh "${server_username}"@"${server_host}" -i "${server_privatekey}" "cd \"${server_extra_addons_path}\" && ls -lah"
+ssh "${server_username}"@"${server_host}" -i "${server_privatekey}" "cd \"${server_extra_addons_path}\" && ls -lah && git logs"
