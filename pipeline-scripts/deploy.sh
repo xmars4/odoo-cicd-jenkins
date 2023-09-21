4d61503846a8af0d79a4880ca79fd5da2078c647
XMars <truong.pham2@sotatek.com>
2023-09-21 17:06:25 +0700

    up

--- pipeline-scripts/deploy.sh
+++ pipeline-scripts/deploy.sh
@@ -2,4 +2,4 @@
 
 source "${WORKSPACE}/pipeline-scripts/utils.sh"
 
-ssh "${server_username}"@"${server_host}" -i $server_privatekey 'ls -lah /opt'
+ssh "${server_username}"@"${server_host}" -i "${server_privatekey}" 'ls -lah /opt'
