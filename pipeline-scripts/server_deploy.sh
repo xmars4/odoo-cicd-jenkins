10bc23ae159ede10ea203f6ca68d37fb8fa8982c
XMars <truong.pham2@sotatek.com>
2023-09-27 16:12:15 +0700

    up

--- pipeline-scripts/server_deploy.sh
+++ pipeline-scripts/server_deploy.sh
@@ -1,61 +1,89 @@
 #!/bin/bash
-server_docker_compose_path=$1
-server_extra_addons_path=$2
-server_config_file=$3
-repo_remote_name="origin"
+server_docker_compose_path=$1 # the path to folder container Odoo docker-compose.yml file
+server_extra_addons_path=$2 # the absolute path to source code, also the git repository
+server_config_file=$3 # the path to Odoo config file
+git_private_key_file=$4 # private key on server use to authenticate on Github
+
+original_repo_remote_name="origin"
+custom_repo_remote_name="origin-ssh"
+custom_repo_host="git-host-ssh"
 EXTRA_ADDONS=
 
-check_current_dir_is_a_git_repo() {
-    git status
+check_git_repo_folder() {
+    cd $server_extra_addons_path
+    git status 2 >/dev/null &>1
     if [[ $? -gt 0 ]]; then
-        echo "Can't execute git commands because current folder is not a git repo!"
+        echo "Can't execute git commands because \"$PWD\" folder is not a git repository!"
         exit 1
     fi
 }
 
-get_git_remote_url() {
-    check_current_dir_is_a_git_repo
-    remote_url=$((git remote get-url $repo_remote_name | head -n 1) 2>/dev/null)
-
-    # provided remote named $repo_remote_name doesn't exist
+get_original_remote_url() {
+    remote_url=$((git remote get-url $original_repo_remote_name | head -n 1) 2>/dev/null)
     if [ -z "$remote_url" ]; then
-        exist_remote_name=$(git remote show | head -n 1)
-        if [ -z "$exist_remote_name" ]; then
+        other_repo_remote_name=$(git remote show | head -n 1)
+        if [ -z "$other_repo_remote_name" ]; then
             return
         fi
-        remote_url=$(git remote get-url $exist_remote_name | head -n 1)
+        remote_url=$(git remote get-url $other_repo_remote_name | head -n 1)
     fi
     echo "$remote_url"
 }
 
-setup_git_ssh_remote() {
-    check_current_dir_is_a_git_repo
-    git remote add $repo_remote_name temp_url
-    remote_url=$(get_git_remote_url)
-    remote_ssh_reg="^git@"
+add_custom_repo_remote() {
+    repo_name=$1
+    custom_remote_url="git@$custom_repo_host:$repo_name"
+    git remote add $custom_repo_remote_name $custom_remote_url
+    git remote set-url $custom_repo_remote_name $custom_remote_url
+}
 
-    if ! [[ $remote_url =~ $remote_ssh_reg ]]; then
-        # the remote url is http(s) url
-        repo_name=$(echo "$remote_url" | sed "s/.*:\/\/[^/]*\///")
+write_custom_git_host_to_ssh_config() {
+    original_repo_host=$1
+    ssh_folder=$HOME/.ssh
+    if [[ ! -d "$ssh_folder" ]]; then
+        mkdir -p "$ssh_folder"
+    fi
+
+    config_value="
+\n# Custom git host for CI/CD process
+Host $custom_repo_host
+  Hostname $original_repo_host
+  IdentityFile $git_private_key_file
+  IdentitiesOnly yes\n
+    "
+    if ! grep -q "Host $custom_repo_host" "$ssh_folder/config"; then
+        echo -e "$config_value" >>"$ssh_folder/config"
+    fi
+}
+
+setup_git_ssh_remote() {
+    remote_url=$(get_original_remote_url)
+    if ! [[ $remote_url =~ ^git@ ]]; then
+        repo_name=$(echo "$remote_url" | sed "s/.*:\/\/[^/]*\///" | sed "s/\.git$//")
         repo_host=$(echo "$remote_url" | sed "s/\/[^/]*\/[^/]*$//" | sed "s/^.*\/\///")
         # re-build repo's ssh url
         # so we can setup and use git command authenticate by ssh private key
         remote_url="git@$repo_host:$repo_name"
+    else
+        repo_name=$(echo "$remote_url" | sed "s/.*://" | sed "s/\.git$//")
+        repo_host=$(echo "$remote_url" | sed "s/^git@//" | sed "s/:.*//")
     fi
-    git remote set-url $repo_remote_name $remote_url
+    add_custom_repo_remote $repo_name
+    write_custom_git_host_to_ssh_config $repo_host
 }
 
 pull_latest_code() {
-    # $1 : path to ssh private key to authenticate with Github
-    check_current_dir_is_a_git_repo
-    git fetch
-    if [[ $? -gt 0 ]]; then
-        # fetch command failed because invalid git authentication
-        # setup ssh and re-try
-        setup_git_ssh_remote
-    fi
+    git fetch $original_repo_remote_name
     current_branch=$(git branch --show-current)
-    ssh-agent bash -c "ssh-add \"$1\"; git pull $repo_remote_name current_branch"
+
+    if [[ $? -gt 0 ]]; then
+        # fetch command failed 
+        # => we have to add custom remote name here and re-fetch
+        setup_git_ssh_remote
+    else
+        git pull $original_repo_remote_name  $current_branch
+    fi
+    git pull $custom_repo_remote_name $current_branch
 }
 
 get_list_addons() {
@@ -85,10 +113,13 @@ update_odoo_services() {
 }
 
 main() {
+    check_git_repo_folder
     pull_latest_code
-    set_list_addons
-    update_config_file
-    update_odoo_services
+    # set_list_addons
+    # update_config_file
+    # update_odoo_services
 }
 
-main
+# main
+# check_git_repo_folder
+echo $(get_original_remote_url)
