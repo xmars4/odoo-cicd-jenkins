5fb338ecc36feeb76ea7db47a5ff9f7fea4a3b0e
XMars <truong.pham2@sotatek.com>
2023-10-02 10:56:48 +0700

    up

--- pipeline-scripts/unit-test.sh
+++ pipeline-scripts/unit-test.sh
@@ -10,7 +10,8 @@ function analyze_log {
     # in case Odoo don't have any ERROR -> log file will be not generated
     # so no need to analyze log anymore
     docker exec $ODOO_CONTAINER_ID sh -c "[ -f ${LOG_FILE} ]"
-    # FIXMe: remove below line #14
+    # FIXMe: remove below line #14,15
+    echo "can u see this log, checkkkk"
     docker exec $ODOO_CONTAINER_ID sh -c "cat ${LOG_FILE}"
     if [ $? != 0 ]; then
         return 0
