ee633e0bdc0a8be41eb278f9578daf1c137414af
xmars <google@tesla.meta>
2023-10-11 23:06:23 +0700

    update script

--- pipeline-scripts/unit-test.sh
+++ pipeline-scripts/unit-test.sh
@@ -23,6 +23,7 @@ function analyze_log {
     echo $LOG_FILE
     echo $LOG_FILE_OUTSIDE
     echo $ODOO_CONTAINER_ID
+    echo $(date)
 
     if [ $error_exist -eq 0 ]; then
         # we copied the log file to Jenkins instance so we can send it to Telegram
