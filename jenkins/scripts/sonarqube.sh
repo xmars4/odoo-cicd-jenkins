b3ba7b1e9fcf1edea6b19c9a75a9897fa3ed836b
XMars <truong.pham2@sotatek.com>
2023-10-18 10:22:48 +0700

    update

--- jenkins/scripts/sonarqube.sh
+++ jenkins/scripts/sonarqube.sh
@@ -2,4 +2,6 @@
 
 source "${PIPELINE_UTILS_SCRIPT_PATH}"
 
-${sonarqubeScannerHome}/bin/sonar-scanner -e -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectName=sonarqube-odoo -Dsonar.projectKey=GS -Dsonar.projectBaseDir="${ODOO_WORKSPACE}/extra-addons"
+[ -z $SONAR_URL ] && SONAR_URL=http://sonarqube:9000
+scanner_result=$(${SONARQUBE_SCANNER_HOME}/bin/sonar-scanner -e -Dsonar.host.url=$SONAR_URL -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectName=sonarqube-odoo -Dsonar.projectKey=GS -Dsonar.projectBaseDir="${ODOO_WORKSPACE}/extra-addons")
+echo $scanner_result
