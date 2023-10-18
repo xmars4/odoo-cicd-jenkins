b8b1aa69420e8843cf94b96e8660015329a682b8
XMars <truong.pham2@sotatek.com>
2023-10-18 10:59:46 +0700

    update

--- jenkins/scripts/sonarqube.sh
+++ jenkins/scripts/sonarqube.sh
@@ -8,5 +8,5 @@ PROJECT_KEY="ODOO_CICD"
 ${SONARQUBE_SCANNER_HOME}/bin/sonar-scanner -e -Dsonar.host.url=$SONAR_URL -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectName=sonarqube-odoo -Dsonar.projectKey=$PROJECT_KEY -Dsonar.projectBaseDir="${ODOO_WORKSPACE}/extra-addons" >/dev/null 2>&1
 
 result_url="$SONAR_URL/dashboard?id=$PROJECT_KEY"
-result_url="${result_url//\/\//\/}"
+result_url=$(echo $result_url | sed "s/\/\//\//2")
 echo "$result_url"
