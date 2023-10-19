11b67eb16787bf6df8af302e6bd8841fd5d0d0ac
XMars <truong.pham2@sotatek.com>
2023-10-19 17:31:55 +0700

    update pylint test

--- .cicd/jenkins/scripts/pylint-test.sh
+++ .cicd/jenkins/scripts/pylint-test.sh
@@ -7,6 +7,8 @@ function analyze_log {
     # we get all info from this test -> just send log file to somewhere
     # no need to analyze anything
     result="empty"
+    cat $CONFIG_FILE
+    cat $LOG_FILE_OUTSIDE
 }
 
 analyze_log
